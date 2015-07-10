d3242bd861da0fb6df3903b048d7820428068659
TarCode <taariq@projectcodex.co>
2015-07-10 13:15:51 +0200

    add category form and route method

--- flaskApp.py
+++ flaskApp.py
@@ -19,14 +19,7 @@ app.config['MYSQL_DB'] = 'nelisaFlask'
 
 @app.route('/')
 def show_landing():
-	if request.method == 'POST':
-		data = {username: request.form['username'],
-				password: request.form['password']}
-		cur = mysql.connection.cursor()
-		cur.execute('''INSERT INTO users SET %s ''' %data)
-		entries = [dict(password=row[0]) for row in cur.fetchall()]
-	else:	
-    		return render_template('land.html')
+    return render_template('land.html')
 
 @app.route('/signUp', methods = ['GET', 'POST'])
 def signUp():
@@ -39,8 +32,8 @@ def signUp():
 			return redirect(url_for('login'))
 		else:
 			return render_template('signUp.html', msg = "Passwords do not match")
-	else:	
-    		return render_template('signUp.html')    
+	else:
+    		return render_template('signUp.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -48,7 +41,7 @@ def login():
 		cur = mysql.connection.cursor()
 		cur.execute('''SELECT password FROM users WHERE username = \"%s\" ''' %request.form['username'])
 		entries = [dict(password=row[0]) for row in cur.fetchall()]
-		
+
 		if entries != [] and request.form['username'] != None and request.form['password'] != None:
 			pw_hash = entries[0]['password']
 			if bcrypt.check_password_hash(pw_hash, request.form['password']):
@@ -58,14 +51,14 @@ def login():
 			elif request.form['password'] != entries[0]['password']:
 				return render_template('login.html', msg = "Incorrect Login")
 	    	else:
-	    		return render_template('login.html', msg = "Incorrect Login")			
+	    		return render_template('login.html', msg = "Incorrect Login")
 	else:
 		if session:
 			return redirect(url_for('show_menu'))
 		else:
 			return render_template('login.html')
 	    		return render_template('login.html')
-        
+
 
 @app.route('/menu')
 def show_menu():
@@ -78,19 +71,31 @@ def show_products():
 	entries = [dict(prod_id=row[0], prod_name=row[1], cat_id=row[2]) for row in cur.fetchall()]
     	return render_template('show_products.html', entries=entries)
 
-@app.route('/categories')
+@app.route('/categories', methods=['GET', 'POST'])
 def show_categories():
-	cur = mysql.connection.cursor()
-	cur.execute('''SELECT * FROM category''')
-	entries = [dict(cat_id=row[0], cat_name=row[1]) for row in cur.fetchall()]
-    	return render_template('show_categories.html', entries=entries)    
+    if request.method == 'POST':
+    	if(request.form['cat_name'] != ""):
+    			cur = mysql.connection.cursor()
+    			cur.execute('''INSERT INTO category VALUES (%s) ''' %(request.form['cat_name']))
+    			mysql.connection.commit()
+    			return redirect(url_for('show_categories'))
+        else:
+                cur = mysql.connection.cursor()
+            	cur.execute('''SELECT * FROM category''')
+            	entries = [dict(cat_id=row[0], cat_name=row[1]) for row in cur.fetchall()]
+                return render_template('show_categories.html', entries=entries, msg = "field cannot be blank")
+    else:
+        	cur = mysql.connection.cursor()
+        	cur.execute('''SELECT * FROM category''')
+        	entries = [dict(cat_id=row[0], cat_name=row[1]) for row in cur.fetchall()]
+            	return render_template('show_categories.html', entries=entries)
 
 @app.route('/suppliers')
 def show_suppliers():
 	cur = mysql.connection.cursor()
 	cur.execute('''SELECT * FROM supplier''')
 	entries = [dict(supplier_id=row[0], supplier_name=row[1]) for row in cur.fetchall()]
-    	return render_template('show_suppliers.html', entries=entries)    	    		
+    	return render_template('show_suppliers.html', entries=entries)
 
 @app.route('/sales')
 def show_sales():
@@ -104,7 +109,7 @@ def show_purchases():
 	cur = mysql.connection.cursor()
 	cur.execute('''SELECT purchase_id, prod_name, date, quantity, cost, totalCost from purchases, product WHERE purchases.prod_id = product.prod_id order by purchase_id''')
 	entries = [dict(purchase_id=row[0], prod_name=row[1], date = row[2], quantity = row[3], cost = row[4], totalCost = row[5]) for row in cur.fetchall()]
-    	return render_template('show_purchases.html', entries=entries)    	
+    	return render_template('show_purchases.html', entries=entries)
 
 @app.route('/clear')
 def clearsession():
@@ -114,7 +119,7 @@ def clearsession():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-	
-	app.run(debug=True, 
+
+	app.run(debug=True,
 	host="172.18.0.224",
     port=int("5000"))
